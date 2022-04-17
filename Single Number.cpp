f1b758f63881e69f078b20904fc0b586c0109942
anothertryatthis <103529716+anothertryatthis@users.noreply.github.com>
2022-04-17 22:47:55 +0530

    Create Single Number.cpp

---
+++ Single Number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 1; i < nums.size(); i = i +2){
+            
+            if(nums[i] != nums[i-1]){
+                return nums[i-1];
+            }
+        }
+        
+        return nums[nums.size() - 1];
+    }
+};
+
+
+//THE XOR solution, someone else's method
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
+    }
+};
